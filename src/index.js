041086a02b7d46984026fd0d9cc3643912909dcf
Jake Champion <jakechampion.jake2@googlemail.com>
2016-04-15 12:33:35 +0100

    move priv vars

--- src/index.js
+++ src/index.js
@@ -9,32 +9,26 @@ function getPercentageViewable (element) {
   return percentage === Infinity ? 0 : percentage;
 
 }
+var s;
+var c;
+module.exports = class ScrollTracker {
+  constructor({element, buckets, callback, delay}) {
+    this.rootEl = element;
+    this.rootEl.dataset.scrollTracking = true;
+    s = throttle(() => {
+      const percentage = getPercentageViewable(element);
+      const currentBuckets = buckets.filter(bucket => bucket <= percentage);
+      if (!isEqual(currentBuckets, c)) {
+        c = currentBuckets;
+        callback(currentBuckets);
+      }
+    }, delay);
+    document.addEventListener('scroll', s);
+  }
 
-module.exports = (function() {
-  var s;
-  var c;
-
-  class ScrollTracker {
-    constructor({element, buckets, callback, delay}) {
-      this.rootEl = element;
-      this.rootEl.dataset.scrollTracking = true;
-      s = throttle(() => {
-        const percentage = getPercentageViewable(element);
-        const currentBuckets = buckets.filter(bucket => bucket <= percentage);
-        if (!isEqual(currentBuckets, c)) {
-          c = currentBuckets;
-          callback(currentBuckets);
-        }
-      }, delay);
-      document.addEventListener('scroll', s);
-    }
-
-    destroy () {
-      document.removeEventListener('scroll', s);
-      delete this.rootEl.dataset.scrollTracking;
-      delete this.rootEl;
-    }
-  };
-
-  return ScrollTracker;
-}());
+  destroy () {
+    document.removeEventListener('scroll', s);
+    delete this.rootEl.dataset.scrollTracking;
+    delete this.rootEl;
+  }
+};
